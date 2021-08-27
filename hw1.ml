0c741be0e0be9a12bbb0c4d1043d4c4d1b98c4cb
jasonpereira96 <jasonpereira074@gmail.com>
2021-08-26 23:18:15 -0500

    updating hw1

--- hw1.ml
+++ hw1.ml
@@ -25,14 +25,10 @@ let rec tree_size (t : tree) : int = match t with
 
 (* question 5 *)
 (* A function that applies function f to every node in tree t *)
-let rec tree_map t f : unit = match t with
-  | Empty -> ()
-  | Leaf (x) -> f x; ();
-  | Node (left, right, value) -> 
-      tree_map left f;
-      f value;
-      tree_map right f;
-      ();;
+let rec tree_map (t: tree) f : tree = match t with
+  | Empty -> Empty
+  | Leaf (x) -> Leaf(f x);
+  | Node (left, right, value) -> Node (tree_map left f, tree_map right f, f value);;
 
 (* --------------------------------------------------------------------------------------- *)
 (* Test cases *)
@@ -57,13 +53,31 @@ let _2 = Node(_4, _5, 2);;
 let _3 = Node(_6, _7, 3);;
 let root = Node(_2, _3, 1);;
 
-(* mapper function *)
-let fn (x : int) : int = 
-  print_int (x * 2);
-  x * 2;;
 
 (* question 4 *)
 print_int (tree_size (root));;
 
+
 (* question 5 *)
-tree_map root fn;;
+
+(* utility function to print a tree *)
+let rec print_tree (t: tree) : unit = match t with
+  | Empty -> ()
+  | Leaf(x) -> 
+      print_int x;
+      print_string "|"
+  | Node(left, right, value) -> 
+      print_tree left;
+      print_int value;
+      print_string "|";
+      print_tree right;;
+      
+(* mapper function *)
+let callback (x : int) : int = x * 2;;
+
+(* printing original tree *)
+print_tree root;;
+(* creating a new tree using tree_map *)
+let new_tree = tree_map root callback;;
+(* printing the new tree *)
+print_tree new_tree;;
