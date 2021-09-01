cea12f5f98b2c3f1b751877f6b9719521f98c4e6
jasonpereira96 <jasonpereira074@gmail.com>
2021-08-31 21:03:29 -0500

    removing Empty value

--- hw1.ml
+++ hw1.ml
@@ -12,23 +12,20 @@ add2 (6, -1);; (* should return 5*)
 (* question 3 *)
 (* Data type to represent a tree *)
 type tree = 
-  | Empty
   | Leaf of int
-  | Node of tree * tree * int;;
+  | Node of tree * tree;;
 
 (* question 4 *)
 (* A function to count the number of nodes in a tree *)
 let rec tree_size (t : tree) : int = match t with
-  | Empty -> 0
   | Leaf (x) -> 1
-  | Node (left, right, value) -> tree_size left + tree_size right + 1;;
+  | Node (left, right) -> tree_size left + tree_size right + 1;;
 
 (* question 5 *)
 (* A function that applies function f to every node in tree t *)
 let rec tree_map (t: tree) f : tree = match t with
-  | Empty -> Empty
   | Leaf (x) -> Leaf(f x);
-  | Node (left, right, value) -> Node (tree_map left f, tree_map right f, f value);;
+  | Node (left, right) -> Node (tree_map left f, tree_map right f);;
 
 (* --------------------------------------------------------------------------------------- *)
 (* Test cases *)
@@ -49,9 +46,9 @@ let _4 = Leaf(4);;
 let _5 = Leaf(5);;
 let _6 = Leaf(6);;
 let _7 = Leaf(7);;
-let _2 = Node(_4, _5, 2);;
-let _3 = Node(_6, _7, 3);;
-let root = Node(_2, _3, 1);;
+let _2 = Node(_4, _5);;
+let _3 = Node(_6, _7);;
+let root = Node(_2, _3);;
 
 
 (* question 4 *)
@@ -62,13 +59,11 @@ print_int (tree_size (root));;
 
 (* utility function to print a tree *)
 let rec print_tree (t: tree) : unit = match t with
-  | Empty -> ()
   | Leaf(x) -> 
       print_int x;
       print_string "|"
-  | Node(left, right, value) -> 
+  | Node(left, right) -> 
       print_tree left;
-      print_int value;
       print_string "|";
       print_tree right;;
       
